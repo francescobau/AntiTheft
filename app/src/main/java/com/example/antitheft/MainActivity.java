864fd63969697357fd39293dbb96dc711e27b5a6
Alberto Ursino <alberto.ursino@studentri.unipd.it>
2020-02-25 15:45:28 +0100

    Skeleton
    Implemented JitPack

--- app/src/main/java/com/example/antitheft/MainActivity.java
+++ app/src/main/java/com/example/antitheft/MainActivity.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.eis.smslibrary.SMSMessage;
+import com.example.antitheft.structure.GPSCommandHandler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO
+
+    }
+
+    /**
+     * //TODO
+     *
+     * @param smsMessage
+     */
+    public void sendCommand(SMSMessage smsMessage){
+        //TODO controllo per il comando: deve essere valido
+        new GPSCommandHandler().sendCommand(smsMessage);
+    }
+
+    /**
+     * //TODO
+     *
+     * @param smsMessage
+     */
+    public void isValidCommand(SMSMessage smsMessage){
+        //TODO
     }
 }

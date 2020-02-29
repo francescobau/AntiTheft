3c700e385768461b9ae52c446083344fd17a3bf8
francescobau <francescobau@users.noreply.github.com>
2020-02-29 23:43:00 +0100

    Improved testing in GPSCommandHandlerTest and in GPSCommandReceiverTest, added @ author and @ since in testing classes.
    KNOWN ISSUES:
    - Missing specifications in testing classes.
    - onCommandReceived() test failes, because the test force-closes the app, and getCurrentLocation() can't work if MainActivity is closed (it needs an Activity instance to work).

--- app/src/androidTest/java/com/example/antitheft/structure/GPSCommandHandlerTest.java
+++ app/src/androidTest/java/com/example/antitheft/structure/GPSCommandHandlerTest.java
@@ -6,44 +6,48 @@ import com.eis.smslibrary.SMSPeer;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Francesco Bau'
+ * @since 29/02/2020
+ */
 public class GPSCommandHandlerTest {
 
     private SMSMessage smsMessage;
     private SMSPeer smsPeer;
 
-    private static final String LOCATION = "A";
-    private static final String VALID_TELEPHONE_NUMBER = "+393515186755";
-    private static final String TEXT_MESSAGE = "A";
+    static final String LOCATION = "A";
+    static final String VALID_TELEPHONE_NUMBER = "+393515186755";
+    static final String TEXT_MESSAGE = "A";
 
     @Before
-    public void init(){
+    public void init() {
         smsPeer = new SMSPeer(VALID_TELEPHONE_NUMBER);
-        smsMessage = new SMSMessage(smsPeer,TEXT_MESSAGE);
+        smsMessage = new SMSMessage(smsPeer, TEXT_MESSAGE);
     }
 
     @Test(expected = NullPointerException.class)
-    public void sendNullMessage(){
+    public void sendNullMessage() {
         new GPSCommandHandler().sendMessage(null);
     }
 
     @Test(expected = NullPointerException.class)
-    public void onNullCommandReceived(){
+    public void onNullCommandReceived() {
         new GPSCommandHandler().onCommandReceived(null);
     }
 
     @Test(expected = NullPointerException.class)
-    public void sendLocationToNullPeer(){
-        new GPSCommandHandler().sendLocation(null,LOCATION);
+    public void sendLocationToNullPeer() {
+        new GPSCommandHandler().sendLocation(null, LOCATION);
     }
 
     @Test
-    public void onCommandReceived(){
+    public void onCommandReceived() {
         new GPSCommandHandler().onCommandReceived(smsMessage);
     }
 
     @Test
-    public void sendLocation(){
-        new GPSCommandHandler().sendLocation(smsPeer,LOCATION);
+    public void sendLocation() {
+        new GPSCommandHandler().sendLocation(smsPeer, LOCATION);
     }
 
 }
--- app/src/androidTest/java/com/example/antitheft/structure/GPSCommandReceiverTest.java
+++ app/src/androidTest/java/com/example/antitheft/structure/GPSCommandReceiverTest.java
@@ -1,5 +1,35 @@
 package com.example.antitheft.structure;
 
-public class GPSCommandReceiverTest {
+import com.eis.smslibrary.SMSMessage;
+import com.eis.smslibrary.SMSPeer;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.example.antitheft.structure.GPSCommandHandlerTest.TEXT_MESSAGE;
+import static com.example.antitheft.structure.GPSCommandHandlerTest.VALID_TELEPHONE_NUMBER;
+
+/**
+ * @author Francesco Bau'
+ * @since 29/02/2020
+ */
+public class GPSCommandReceiverTest {
+    private SMSMessage smsMessage;
+    private SMSPeer smsPeer;
+
+    @Before
+    public void init() {
+        smsPeer = new SMSPeer(VALID_TELEPHONE_NUMBER);
+        smsMessage = new SMSMessage(smsPeer, TEXT_MESSAGE);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void onNullMessageReceived() {
+        new GPSCommandReceiver().onMessageReceived(null);
+    }
+
+    @Test
+    public void onMessageReceived() {
+        new GPSCommandReceiver().onMessageReceived(smsMessage);
+    }
 }

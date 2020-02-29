c37a09f7728c030986e0fbea945e4670953f041e
francescobau <francescobau@users.noreply.github.com>
2020-03-01 00:18:50 +0100

    Inserted warning in VALID_TELEPHONE_NUMBER, edited GPSCommandReceiverTest, added specifications in testing classes.

--- app/src/androidTest/java/com/example/antitheft/structure/GPSCommandHandlerTest.java
+++ app/src/androidTest/java/com/example/antitheft/structure/GPSCommandHandlerTest.java
@@ -15,36 +15,86 @@ public class GPSCommandHandlerTest {
     private SMSMessage smsMessage;
     private SMSPeer smsPeer;
 
-    static final String LOCATION = "A";
+    static final String LOCATION = "";
+    // WARNING: insert a VALIID telephoneNumber, because it sends an SMS to that telephone number.
     static final String VALID_TELEPHONE_NUMBER = "+393515186755";
-    static final String TEXT_MESSAGE = "A";
+    static final String TEXT_MESSAGE = "";
 
+
+    /**
+     * Initialization of the fields, before testing.
+     *
+     * @see com.eis.smslibrary.SMSPeer
+     * @see com.eis.smslibrary.SMSMessage
+     */
     @Before
     public void init() {
         smsPeer = new SMSPeer(VALID_TELEPHONE_NUMBER);
         smsMessage = new SMSMessage(smsPeer, TEXT_MESSAGE);
     }
 
+    /**
+     * Test what happens if there's an attempt to send a null {@link SMSMessage} .
+     * <p>
+     * {@link NullPointerException} is expected to be thrown, because of the @NonNull annotation.
+     *
+     * @see NullPointerException
+     * @see com.eis.smslibrary.SMSMessage
+     * @see GPSCommandHandler#sendMessage(SMSMessage)
+     */
     @Test(expected = NullPointerException.class)
     public void sendNullMessage() {
         new GPSCommandHandler().sendMessage(null);
     }
 
+    /**
+     * Test what happens if a null {@link SMSMessage} is received in method
+     * {@link GPSCommandHandler#onCommandReceived(SMSMessage)}.
+     * <p>
+     * {@link NullPointerException} is expected to be thrown, because of the @NonNull annotation.
+     *
+     * @see NullPointerException
+     * @see com.eis.smslibrary.SMSMessage
+     * @see GPSCommandHandler#onCommandReceived(SMSMessage)
+     */
     @Test(expected = NullPointerException.class)
     public void onNullCommandReceived() {
         new GPSCommandHandler().onCommandReceived(null);
     }
 
+    /**
+     * Test what happens if there's an attempt to call {@link GPSCommandHandler#sendLocation(SMSPeer, String)}
+     * with a null {@link SMSPeer}.
+     * <p>
+     * {@link NullPointerException} is expected to be thrown, because of the @NonNull annotation.
+     *
+     * @see NullPointerException
+     * @see com.eis.smslibrary.SMSPeer
+     * @see GPSCommandHandler#sendLocation(SMSPeer, String)
+     */
     @Test(expected = NullPointerException.class)
     public void sendLocationToNullPeer() {
         new GPSCommandHandler().sendLocation(null, LOCATION);
     }
 
+    /**
+     * Test what happens when a certain {@link SMSMessage}, supposed to be a command, is received by
+     * method {@link GPSCommandHandler#onCommandReceived(SMSMessage)}.
+     *
+     * @see com.eis.smslibrary.SMSMessage
+     * @see GPSCommandHandler#onCommandReceived(SMSMessage)
+     */
     @Test
     public void onCommandReceived() {
         new GPSCommandHandler().onCommandReceived(smsMessage);
     }
 
+    /**
+     * Test method {@link GPSCommandHandler#sendLocation(SMSPeer, String)}.
+     *
+     * @see com.eis.smslibrary.SMSPeer
+     * @see GPSCommandHandler#sendLocation(SMSPeer, String)
+     */
     @Test
     public void sendLocation() {
         new GPSCommandHandler().sendLocation(smsPeer, LOCATION);
--- app/src/androidTest/java/com/example/antitheft/structure/GPSCommandReceiverTest.java
+++ app/src/androidTest/java/com/example/antitheft/structure/GPSCommandReceiverTest.java
@@ -6,30 +6,68 @@ import com.eis.smslibrary.SMSPeer;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.example.antitheft.MainActivity.FULL_DEFAULT_LOCATE_COMMAND;
 import static com.example.antitheft.structure.GPSCommandHandlerTest.TEXT_MESSAGE;
 import static com.example.antitheft.structure.GPSCommandHandlerTest.VALID_TELEPHONE_NUMBER;
 
 /**
+ * Test of {@link GPSCommandReceiver} class.
+ *
  * @author Francesco Bau'
+ * @see GPSCommandReceiver
  * @since 29/02/2020
  */
 public class GPSCommandReceiverTest {
     private SMSMessage smsMessage;
     private SMSPeer smsPeer;
 
+    /**
+     * Initialization of the fields, before testing.
+     *
+     * @see com.eis.smslibrary.SMSPeer
+     * @see com.eis.smslibrary.SMSMessage
+     */
     @Before
     public void init() {
         smsPeer = new SMSPeer(VALID_TELEPHONE_NUMBER);
         smsMessage = new SMSMessage(smsPeer, TEXT_MESSAGE);
     }
 
-    @Test(expected = NullPointerException.class)
+    /**
+     * Tests what happens if the listener receives a null message.
+     * Nothing bad should happen, since it's @Nullable, and method
+     * {@link GPSCommandReceiver#onMessageReceived(SMSMessage)} is supposed to completely ignore
+     * null {@link SMSMessage} parameters.
+     *
+     * @see com.eis.smslibrary.SMSMessage
+     * @see GPSCommandReceiver#onMessageReceived(SMSMessage)
+     */
+    @Test
     public void onNullMessageReceived() {
         new GPSCommandReceiver().onMessageReceived(null);
     }
 
+    /**
+     * Tests what happends if the listener receives a certain {@link SMSMessage}.
+     *
+     * @see com.eis.smslibrary.SMSMessage
+     * @see GPSCommandReceiver#onMessageReceived(SMSMessage)
+     */
     @Test
     public void onMessageReceived() {
         new GPSCommandReceiver().onMessageReceived(smsMessage);
+    }
+
+    /**
+     * Almost the same as method {@link #onMessageReceived()}, but it tests what
+     * happens if a valid LOCATE command is received.
+     *
+     * @see com.eis.smslibrary.SMSMessage
+     * @see GPSCommandReceiver#onMessageReceived(SMSMessage)
+     */
+    @Test
+    public void onValidLocateCommandReceived() {
+        SMSMessage message = new SMSMessage(smsPeer, FULL_DEFAULT_LOCATE_COMMAND);
+        new GPSCommandReceiver().onMessageReceived(message);
     }
 }
